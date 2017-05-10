c7fe61e274c99ffeba7a8cc39fc12661d7e9cb52
Fortika <andreas@fortika.se>
2017-05-10 07:36:21 +0200

    export of functions

--- Fortika.Utils.psm1
+++ Fortika.Utils.psm1
@@ -2,3 +2,6 @@
 # dot-source functions
 . ${PSScriptRoot}\functions\New-FortikaPSFunction.ps1
 . ${PSScriptRoot}\functions\Install-FortikaUtils.ps1
+
+# make cmdlets available by exporting them.
+Get-ChildItem function: | ? { $_.Name -like '*Fortika*' -and $_.Name -notlike '_*' } | Select Name | ForEach-Object { Export-ModuleMember -Function $_.Name }
